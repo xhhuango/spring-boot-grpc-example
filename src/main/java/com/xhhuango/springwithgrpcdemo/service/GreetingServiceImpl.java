9f1182c03082778b6c863314b7579cd2d29e4c21
Wayne <xhhuango@gmail.com>
2018-11-07 14:31:23 +0800

    Add request response stream

--- src/main/java/com/xhhuango/springwithgrpcdemo/service/GreetingServiceImpl.java
+++ src/main/java/com/xhhuango/springwithgrpcdemo/service/GreetingServiceImpl.java
@@ -5,6 +5,9 @@ import com.xhhuango.springwithgrpcdemo.GreetingServiceOuterClass;
 import com.xhhuango.springwithgrpcdemo.starter.GrpcService;
 import io.grpc.stub.StreamObserver;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @GrpcService
 public class GreetingServiceImpl extends GreetingServiceGrpc.GreetingServiceImplBase {
     @Override
@@ -33,11 +36,11 @@ public class GreetingServiceImpl extends GreetingServiceGrpc.GreetingServiceImpl
     public StreamObserver<GreetingServiceOuterClass.HelloRequest> greetingWithRequestStream(
             StreamObserver<GreetingServiceOuterClass.HelloResponse> responseObserver) {
         return new StreamObserver<GreetingServiceOuterClass.HelloRequest>() {
-            private StringBuffer stringBuffer = new StringBuffer();
+            private List<String> nameList = new ArrayList<>();
 
             @Override
             public void onNext(GreetingServiceOuterClass.HelloRequest request) {
-                stringBuffer.append(request.getName()).append(" ");
+                nameList.add(request.getName());
             }
 
             @Override
@@ -48,16 +51,37 @@ public class GreetingServiceImpl extends GreetingServiceGrpc.GreetingServiceImpl
             @Override
             public void onCompleted() {
                 GreetingServiceOuterClass.HelloResponse response = GreetingServiceOuterClass.HelloResponse.newBuilder()
-                        .setGreeting("(Stream Request) Hello there, " + stringBuffer.toString())
+                        .setGreeting("(Stream Request) Hello there, " + String.join(" ", nameList))
                         .build();
                 responseObserver.onNext(response);
+                responseObserver.onCompleted();
             }
         };
     }
 
-//    @Override
-//    public StreamObserver<GreetingServiceOuterClass.HelloRequest> greetingWithRequestResponseStream(
-//            StreamObserver<GreetingServiceOuterClass.HelloResponse> responseObserver) {
-//
-//    }
+    @Override
+    public StreamObserver<GreetingServiceOuterClass.HelloRequest> greetingWithRequestResponseStream(
+            StreamObserver<GreetingServiceOuterClass.HelloResponse> responseObserver) {
+        return new StreamObserver<GreetingServiceOuterClass.HelloRequest>() {
+            private List<String> nameList = new ArrayList<>();
+
+            @Override
+            public void onNext(GreetingServiceOuterClass.HelloRequest request) {
+                nameList.add(request.getName());
+            }
+
+            @Override
+            public void onError(Throwable t) {
+                t.printStackTrace();
+            }
+
+            @Override
+            public void onCompleted() {
+                nameList.stream()
+                        .map(name -> GreetingServiceOuterClass.HelloResponse.newBuilder().setGreeting("(Stream Request/Response) Hello there, " + name).build())
+                        .forEach(responseObserver::onNext);
+                responseObserver.onCompleted();
+            }
+        };
+    }
 }
